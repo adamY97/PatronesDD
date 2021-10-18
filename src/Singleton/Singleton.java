5fa2fcdabead6cfc75b5f522e4b92d08a1fd20b8
adamY97 <adam24891@gmail.com>
2021-10-18 10:56:41 +0200

    Changes

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -4,13 +4,13 @@ public class Singleton {
 	private String nombre;
 	private static Singleton singleton;
 	
-	private Singleton (String pNombre) {
+	private Singleton (String nombre) {
 		this.nombre = nombre;
 	}
 
-	public static Singleton getInstance(String pNombre) {
+	public static Singleton getInstance(String nombre) {
 		if (singleton == null) {
-			singleton = new Singleton(pNombre);
+			singleton = new Singleton(nombre);
 		} else {
 			return null;
 		}
